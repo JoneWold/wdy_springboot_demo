7e857d6fe6321c57f06c925ab42d0b523201da31
wgch <1153540212@qq.com>
2020-07-07 12:55:46 +0800

    第nn次版本提交测试

--- src/main/java/com/wdy/springboot/biz/controller/HelloController.java
+++ src/main/java/com/wdy/springboot/biz/controller/HelloController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 
-    @GetMapping("/")
+//    @GetMapping("/")
     public String say() {
         return "Hello Spring Boot";
     }
