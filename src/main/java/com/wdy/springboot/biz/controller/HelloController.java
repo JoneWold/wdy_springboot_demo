8b15638ce9ef2a597da9e0b43d6d3c5881c588d1
wgch <1153540212@qq.com>
2020-07-09 18:01:53 +0800

    第nn次版本提交测试

--- src/main/java/com/wdy/springboot/biz/controller/HelloController.java
+++ src/main/java/com/wdy/springboot/biz/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 /**
  * @author wgch
  * @Description
@@ -13,9 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HelloController {
 
-//    @GetMapping("/")
+    //    @GetMapping("/")
     public String say() {
         return "Hello Spring Boot";
     }
 
+
+    @GetMapping("/thyme")
+    public String thyme(Map<String, Object> map) {
+        // 给request域中方welcome
+        map.put("welcome", "少时诵诗书");
+        // 转到thymeleaf.html
+        return "thymeleaf";
+    }
 }
