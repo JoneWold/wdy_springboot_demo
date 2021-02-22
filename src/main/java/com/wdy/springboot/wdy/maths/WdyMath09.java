041ea2d4144f837927e2dda129d9f22d2bf660a3
wgch <1153540212@qq.com>
2021-02-22 15:33:02 +0800

    第nn次版本提交测试

---
+++ src/main/java/com/wdy/springboot/wdy/maths/WdyMath09.java
@@ -0,0 +1,21 @@
+package com.wdy.springboot.wdy.maths;
+
+/**
+ * @author wgch
+ * @date 2021/2/22
+ */
+public class WdyMath09 {
+
+    public static void main(String[] args) {
+        System.out.println("--------------------------------------------------------------------");
+        System.out.println("【程序9】   题目：一个数如果恰好等于它的因子之和，这个数就称为\"完数\"。例如6=1＋2＋3。编程找出1000以内的所有完数。");
+        System.out.println("--------------------------------------------------------------------");
+        math09();
+    }
+
+    private static void math09() {
+
+
+    }
+
+}
